6ae840513199f5b787a04c01b3c148581904e127
lucasnapomucena <lucasnapomucena@gmail.com>
2020-04-02 01:26:27 -0300

    feat: add multer and create table is avatar

---
+++ src/app/models/File.js
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import Sequelize, { Model } from 'sequelize';
+
+class File extends Model {
+    static init(sequelize) {
+        super.init({
+            name: Sequelize.STRING,
+            path: Sequelize.STRING
+        }, {
+            sequelize,
+        });
+
+        return this;
+    }
+}
+
+export default File;
